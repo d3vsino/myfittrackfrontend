a43ba43319341a8e951537cc9719f83a72d64153
yassin <yassronaldo33@gmail.com>
2025-05-22 15:14:44 +0300

    initial commit

--- project/src/services/auth.js
+++ project/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:8000/api"
+const API_URL = "https://my-fit-track.onrender.com/api"
 
 // Create axios instance with auth header
 export const authAxios = axios.create({
